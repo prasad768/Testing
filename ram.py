d23983a5a6aecbd2b476d420f6e7c88c8d45d208
prasad768 <60334546+prasad768@users.noreply.github.com>
2020-01-28 20:22:02 +0530

    first verson

---
+++ ram.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 28 15:56:07 2020
+
+@author: lenovo
+"""
+no=73
+def Isprime(numb):
+    if(numb<2):
+        return False
+    for i in range(2,numb):
+        if (numb%i==0):
+             return False
+    return True
+
+def sumdigits (numb):
+    return 10
+
+
+if Isprime(no) and sumdigits(no)==10 :
+    print(no," true")
+else:
+    print(no," false")
+    
