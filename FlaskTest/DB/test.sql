dc6259f4b74b7afc682495dc1b37f291b4d57f84
prasad768 <60334546+prasad768@users.noreply.github.com>
2020-02-05 17:07:11 +0530

    opt def table script

---
+++ FlaskTest/DB/test.sql
@@ -0,0 +1,29 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.19 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table test.plan
+CREATE TABLE IF NOT EXISTS `plan` (
+  `planid` int DEFAULT NULL,
+  `atdate` datetime DEFAULT NULL,
+  `strike` decimal(11,2) DEFAULT NULL,
+  `entry` decimal(11,2) DEFAULT NULL,
+  `STOPloss` decimal(11,2) DEFAULT NULL,
+  `target` decimal(11,2) DEFAULT NULL,
+  `risk` varchar(25) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
