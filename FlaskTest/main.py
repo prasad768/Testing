2d5fa595eaaa0c6edcee26d2d12164ae676a6df7
ajaymj22 <60683469+ajaymj22@users.noreply.github.com>
2020-02-06 14:34:45 +0530

    updated list

--- FlaskTest/main.py
+++ FlaskTest/main.py
@@ -20,7 +20,12 @@ def roots():
 
 @app.route('/OptDefList')
 def optdeflist():
-    a={}
-    return render_template('OptDefList.html', data=a)
+    con = connector.connect(user='root',password='123456789',host='localhost',database='test',auth_plugin='mysql_native_password')
+    cursor =con.cursor()
+    cursor.execute("select *  from planning")
+    a=cursor.fetchall()
+    #a={"time":row[1],"strike":row[2],"entry":row[3],"stoploss":row[4],"target":row[5],"risk":row[6]}
+    
+    return render_template('OptDefList1.html', data=a)
 
 app.run()
