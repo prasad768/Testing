cf41c5e5cbed3be20ec57a62fffb6da196e02777
Roopesh <roopesht@gmail.com>
2020-02-06 16:05:12 +0530

    Updated field names

--- FlaskTest/DB/test.sql
+++ FlaskTest/DB/test.sql
@@ -12,29 +12,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping structure for table test.bank1
-CREATE TABLE IF NOT EXISTS `bank1` (
-  `underlying` varchar(50) DEFAULT NULL,
-  `underlyinglevel1` decimal(11,2) DEFAULT NULL,
-  `underlyinglevel2` decimal(11,2) DEFAULT NULL,
-  `underlyinglevel3` decimal(11,2) DEFAULT NULL,
-  `underlyinglevel4` decimal(11,2) DEFAULT NULL,
-  `underlyinglevel5` decimal(11,2) DEFAULT NULL,
-  `notabove` decimal(11,2) DEFAULT NULL,
-  `notbelow` decimal(11,2) DEFAULT NULL,
-  `ivtop` decimal(11,2) DEFAULT NULL,
-  `ivbottom` decimal(11,2) DEFAULT NULL,
-  `weeklyexpirypriceup` decimal(11,2) DEFAULT NULL,
-  `weeklyexpirypricedown` decimal(11,2) DEFAULT NULL,
-  `15mtrend` int DEFAULT NULL,
-  `maxpaindirection` int DEFAULT NULL,
-  `maxpaintarget` decimal(11,2) DEFAULT NULL,
-  `pcrdirection` int DEFAULT NULL,
-  `pcrto` decimal(11,2) DEFAULT NULL,
-  `pcrotherside` decimal(11,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS OptDef (
+  Underlying varchar(10) DEFAULT NULL,
+  Level1 decimal(11,2) DEFAULT NULL,
+  Level2 decimal(11,2) DEFAULT NULL,
+  Level3 decimal(11,2) DEFAULT NULL,
+  Level4 decimal(11,2) DEFAULT NULL,
+  Level5 decimal(11,2) DEFAULT NULL,
+  NotAbove decimal(11,2) DEFAULT NULL,
+  NotBelow decimal(11,2) DEFAULT NULL,
+  IVTop decimal(11,2) DEFAULT NULL,
+  IVBottom decimal(11,2) DEFAULT NULL,
+  WeeklyExpiryTargetUp decimal(11,2) DEFAULT NULL,
+  WeeklyExpiryTargetDown decimal(11,2) DEFAULT NULL,
+  Trend15M int DEFAULT NULL,
+  MaxPainDirection int DEFAULT NULL,
+  MaxPainTarget decimal(11,2) DEFAULT NULL,
+  PCRDirection int DEFAULT NULL,
+  PCRUp decimal(11,2) DEFAULT NULL,
+  PCRDown decimal(11,2) DEFAULT NULL
+);
 
--- Data exporting was unselected.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
