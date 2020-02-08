47da8acf3f8a0ac09887a614c8b800eec7ee8b23
ajaymj22 <60683469+ajaymj22@users.noreply.github.com>
2020-02-08 14:45:20 +0530

    events.sql

---
+++ FlaskTest/DB/events.sql
@@ -0,0 +1,31 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.19 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table test.events
+CREATE TABLE IF NOT EXISTS `events` (
+  `idno` int NOT NULL AUTO_INCREMENT,
+  `newsdate` date DEFAULT NULL,
+  `header` varchar(50) DEFAULT NULL,
+  `Newstype` varchar(50) DEFAULT NULL,
+  `impact` decimal(10,0) DEFAULT NULL,
+  `impactrationale` varchar(50) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  `effectivedate` date DEFAULT NULL,
+  KEY `pk` (`idno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
