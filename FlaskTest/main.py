e3bfb58fb1bb370bc7b5542a48dc86cc2e46b9be
Roopesh <roopesht@gmail.com>
2020-02-10 13:18:56 +0530

    Opt Def Select

--- FlaskTest/main.py
+++ FlaskTest/main.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request
 from OptDef import getOptDef, postOptDef
 from events import getdata, postdata
+from common import getConn
 
 from OptDefList import getOptDefList
 app=Flask(__name__)
@@ -11,6 +12,10 @@ app=Flask(__name__)
 def root():
     return render_template('main.html')
 
+@app.route('/optdeflist')
+def optdeflist():
+    return getOptDefList1 ()
+    
 @app.route('/optdef', methods=['GET', 'POST'])
 def OptDef():
     id = request.args.get("id")
@@ -30,5 +35,12 @@ def table1():
     if request.method == 'POST':
         return postdata()
 
+def getOptDefList1 ():
+    con = getConn()
+    cursor =con.cursor()
+    cursor.execute("select id, underlying  from optdef ")
+    a=cursor.fetchall()
+    #a={"time":row[1],"strike":row[2],"entry":row[3],"stoploss":row[4],"target":row[5],"risk":row[6]}
+    return render_template('OptDef_Select.html', data=a)
 
 app.run()
